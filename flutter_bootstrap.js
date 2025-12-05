095d5b414b1e2474a76db1fbc13d6c06a897d6ca
bastosmichael <bastosmichael@users.noreply.github.com>
2025-12-05 22:36:55 +0000

    deploy: a43273941b9793a7b4d4e3f1dce5d168667a72ce

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "553164659"
+    serviceWorkerVersion: "2198450096"
   }
 });
