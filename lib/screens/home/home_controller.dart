9ce2ec6f874650308de29ff8a257448a1b135a42
Michael Bastos <bastosmichael@gmail.com>
2025-12-06 07:31:12 -0600

    adding a json checker

--- lib/screens/home/home_controller.dart
+++ lib/screens/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeController extends StatefulWidget {
   const HomeController({super.key});
@@ -6,19 +7,53 @@ class HomeController extends StatefulWidget {
   State<HomeController> createState() => _HomeControllerState();
 }
 class _HomeControllerState extends State<HomeController> {
-  @override
-  void initState() {
-    super.initState();
+  String _apiStatus = 'Press the button to check API connection';
+
+  Future<void> _checkApi() async {
+    setState(() {
+      _apiStatus = 'Checking API...';
+    });
+    try {
+      final response =
+          await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+      if (response.statusCode == 200) {
+        setState(() {
+          _apiStatus = 'Success: ${response.body}';
+        });
+      } else {
+        setState(() {
+          _apiStatus = 'Error: ${response.statusCode}';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _apiStatus = 'Exception: $e';
+      });
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-          ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _checkApi,
+                child: const Text('Check API'),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                _apiStatus,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
         ),
       ),
     );
